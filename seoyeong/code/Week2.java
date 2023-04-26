8902e5f07c7622ab07893ceba83020dde435a5e7
김서영 <55133538+ksy990628@users.noreply.github.com>
2023-04-26 23:03:53 +0900

    스택과 큐

--- seoyeong/code/Week2.java
+++ seoyeong/code/Week2.java
@@ -1,11 +1,6 @@
 // 스택과 큐
 // https://school.programmers.co.kr/learn/courses/30/lessons/42583
 
-// 2	10	[7,4,5,6]	8
-// 100	100	[10]	101
-// 100	100	[10,10,10,10,10,10,10,10,10,10]	110
-
-
 import java.util.LinkedList;
 import java.util.Queue;
 
