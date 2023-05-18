66bbde578ca5afec662788c8efebafc5f87e71bb
Jung Yunho <a01049048063@gmail.com>
2023-05-18 22:10:35 +0900

    refactor: POJ60058

--- Yunho/code/week05/Solution.java
+++ Yunho/code/week05/Solution.java
@@ -1,30 +1,35 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/60058%5D
+// https://school.programmers.co.kr/learn/courses/30/lessons/60058
 // 괄호 변환
 
 /**
- * (남은)주어진 p가 "("로 시작하면 균형 잡힌 문자열이 될때까지 u에 저장 나머지는 v에 저장
- * (남은)주어진 P가  ")"로 시작하면 
+ * if (p == "") {  
+ *          return "";
+ *      }
+ * 처음 1번 조건을 이렇게 했다가 계속 stackoverflow 에러가 발생하 원인파악하는데 시간이 오래 걸렸습니다.. 
  */
 import java.util.*;
 class Solution {
     public String solution(String p) {
-        // 1. 빈문자열 반환
-        if(p == ""){
-            return p;
+        // 1단계
+        if (p.isEmpty()) {  
+            return "";
         }
-        // 2. 문자열 분리 
-        int idx = split(p);
+        // 2단계
+        int idx = split(p);  
         
         String u = p.substring(0, idx);
         String v = p.substring(idx);
-
-        // 3. 4 수행 필요 
-        String answer = "";
-        return answer;
+        
+        if (isCorrect(u)) { 
+            return u + solution(v);
+        } else { 
+            String answer = "(" + solution(v) + ")";
+            answer += change(u.substring(1, u.length() - 1));
+            return answer;
+        }
     }
-
-    // 문자열 분리를 위한 함수 
-    public int split(String p) {
+    
+    private int split(String p) {
         int a = 0;
         int b = 0;
         int idx = 0;
@@ -43,5 +48,35 @@ class Solution {
         }
         
         return idx;
+    }
+    
+    private boolean isCorrect(String p) {
+        int count = 0;
+        
+        for (char c : p.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else {
+                if (count == 0) {
+                    return false;
+                }
+                count--;
+            }
+        }
+        
+        return true;
+    }
+    
+    private String change(String p) {
+        char[] arr = p.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(') {
+                arr[i] = ')';
+            } else {
+                arr[i] = '(';
+            }
+        }
+        
+        return String.valueOf(arr);
     }
 }
