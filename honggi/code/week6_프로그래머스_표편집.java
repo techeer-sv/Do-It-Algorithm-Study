1bca4892fd9b59d43606674a067de6ede5e0bbe7
OhHongGi <5ghdrl@naver.com>
2023-05-23 13:54:17 +0900

    refactor: Solving the problem
    Issue num : #48 (week 6 - Common problem solution)

--- honggi/code/week6_프로그래머스_표편집.java
+++ honggi/code/week6_프로그래머스_표편집.java
@@ -2,7 +2,13 @@ import java.util.*;
 class Solution {
     /*
         n - 행의 개수, k - 처음 선택된 행의 위치, cmd - 수행한 명령어들 (U X, D X, C, Z)
-                
+        
+        유의사항
+        1. U & D의 경우, 행의 범위를 넘어가게 되면 circle과 같이 반대쪽 끝으로 돌아옴
+        2. C의 경우, 행 삭제 시 바로 아래 행이 선택됨
+            1) 삭제된 행이 맨 밑인 경우, 바로 윗 행을 선택
+        3. 가장 최근에 삭제된 행을 복구하며, 이 때 현재 선택된 행은 바뀌지 않음
+        
         모든 명령어를 수행한 후, 표의 상태와 처음 주어진 표의 상태를 비교하여
         삭제된 행은 X, 삭제되지 않은 행은 O로 표시한 문자열 반환
     */
@@ -50,12 +56,10 @@ class Solution {
     public int processUp(int index, int move){
         move %= tableL;
         int movedI = index - move;
-        System.out.println("nextIndex: "+ (movedI >= 0 ? movedI : tableL-movedI));
         return movedI >= 0 ? movedI : tableL-movedI;
     }
     
     public int processDown(int index, int move){
-        System.out.println("nextIndex: "+ (index + move) % tableL);
         return (index + move) % tableL;
     }
     
@@ -63,7 +67,6 @@ class Solution {
         int removedI = table.remove(index);
         reset.add(removedI);
         tableL--;
-        printtable();
         if (index == tableL)
             return index-1;
         else
@@ -72,21 +75,28 @@ class Solution {
     
     public int processReset(int index){
         int removedI = reset.pop();
-        if (removedI > tableL)
-            table.add(removedI);
-        else
-            table.add(removedI, removedI);
+        int insertIndex = binarysearch(0, tableL-1, removedI);
+        table.add(insertIndex, removedI);
+
+        if (insertIndex <= index)
+            index++;
+
         tableL++;
-        printtable();
         return index;
     }
     
-    public void printtable(){
-        for (int i = 0; i<tableL; i++){
-            System.out.println(i+": " + table.get(i));
+    public int binarysearch(int l, int r, int value){
+        while (l <= r){
+            int mid = (l+r)/2;
+            if (table.get(mid) < value)
+                l = mid+1;
+            else if (table.get(mid) > value)
+                r = mid-1;
+            else
+                return mid;
         }
-        System.out.println();
+        return l;
     }
-
+    
 
 }
