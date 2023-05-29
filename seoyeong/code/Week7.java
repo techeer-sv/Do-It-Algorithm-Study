b215dc59ddeb12e2df0ea7bfd7186f258763064c
김서영 <55133538+ksy990628@users.noreply.github.com>
2023-05-29 19:19:07 +0900

    [Week7 게임맵최단거리] 최단거리 추가 및 예외 처리 적용

--- seoyeong/code/Week7.java
+++ seoyeong/code/Week7.java
@@ -2,11 +2,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1844
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Week7{
     public static void main(String[]args){
-        System.out.println(solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}}));  // 11
+        // System.out.println(solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}})); // 11
+        System.out.println(solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}})); // -1
         /*
         * 1,0,1,1,1
         * 1,0,1,0,1
@@ -16,37 +16,56 @@ public class Week7{
         * */
     }
 
-    public static int dfs(int[][]maps,int x,int y){
+    public static int searchPath(int[][]maps,int x,int y){
         // 동서남북, x는 행 y는 열
         int [] dx = new int[]{0,0,-1,1};
         int [] dy = new int[]{1,-1,0,0};
-        Queue queue = new LinkedList();
+        LinkedList<Integer[]>queue = new LinkedList<>();
+        queue.add(new Integer[]{x, y});
 
+        while (queue.size()>0){
+            Integer[] pos = queue.pollFirst();
+            x = pos[0];
+            y = pos[1];
 
-        // 이동할 수 있는 값 구하기
-        for (int i=0; i<4; i++){
-            x += dx[i];
-            y += dy[i];
-            if (x >= 0 && x < maps.length && y >= 0 && y < maps[0].length){
-                if (maps[x][y] > 0) {
-                    queue.add(new int[]{x, y});
-                    maps[x][y] += 1;
-                    x -= dx[i];
-                    y -= dy[i];
+            if (x==maps.length-1 && y==maps[0].length-1) break;
+
+            System.out.println("\n"+x+" "+y);
+            for (int i=0; i<maps.length; i++){
+                for (int j=0; j<maps[0].length;j++){
+                    System.out.print(maps[i][j]+" ");
                 }
+                System.out.println("");
+            }
+
+
+
+
+            // 이동할 수 있는 값 구하기
+            for (int i=0; i<4; i++){
+                int newX = x+dx[i];
+                int newY = y+dy[i];
+                if (newX >= 0 && newX < maps.length && newY >= 0 && newY < maps[0].length && maps[newX][newY]>0){
+                    maps[newX][newY] = maps[x][y]+1;
+                    queue.add(new Integer[]{newX, newY});
+                }
+            }
+            maps[x][y] = 0; // 이미 지나온 길은 갈 수 없는 길 (0)으로 만들기
+        }
+
+        int max = 0;
+        for (int i=0; i<maps.length; i++){
+            for (int j=0; j<maps[0].length;j++){
+                if (maps[i][j] > max) max = maps[i][j];
             }
         }
 
-        while (queue.size()>0){
-            int [] pos = (int[]) queue.poll();
-            dfs(maps,pos[0],pos[1]);
-        }
-
-        return maps[x][y];
+        if (maps[maps.length-1][maps[0].length-1] == max && max != 1) return max;
+        return -1;
     }
 
     public static int solution(int[][] maps) {
-        int answer = dfs(maps,0,0);
+        int answer = searchPath(maps,0,0);
         return answer;
     }
 }
