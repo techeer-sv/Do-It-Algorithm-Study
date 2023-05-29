ba302b88292d17726172ed9df43f3a19fbd72767
OhHongGi <5ghdrl@naver.com>
2023-05-29 15:30:30 +0900

    feat: Solve the problem
    Issue num : #48 (week 7 - Common problem solution)

--- honggi/code/week7_프로그래머스_게임맵최단거리.java
+++ honggi/code/week7_프로그래머스_게임맵최단거리.java
@@ -1,11 +1,12 @@
+import java.util.*;
+
 class Solution {
-
-    // 정확도 O, 효율성 X.. 다른 방법 강구해보기
-
     boolean[][] isVisited;
     int rl,cl;
     int[][] drdc = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
     int min = Integer.MAX_VALUE;
+    Queue<int []> child = new LinkedList();
+    
     public int solution(int[][] maps) {
         rl = maps.length;
         cl = maps[0].length;
@@ -17,35 +18,44 @@ class Solution {
                     isVisited[i][j] = true;
             }
         }
-        int result = dfs(0,0,0);
+        
+        int result = bfs(0,0,0);
         
         if (min != Integer.MAX_VALUE)
             return result;
         else
             return -1;
     }
-    
-    public int dfs(int rowI, int columnI, int depth){
-        if (!checkIndex(rowI, columnI) || isVisited[rowI][columnI])
-            return -1;
-        else if (rowI == rl-1 && columnI == cl-1){
-            min = Math.min(depth+1, min);
-            return min;
-        }
+
+    public int bfs(int rowI, int columnI, int depth){
+        child.add(new int[]{0,0,1});
         
-        int result = -1;
-        isVisited[rowI][columnI] = true;
-        for (int[] move : drdc){
-            result = dfs(rowI + move[0], columnI + move[1], depth+1);
-            if (result != -1){
-                min = Math.min(min, result);
+        while(!child.isEmpty()){
+            int[] c = child.remove();
+            if (isVisited[c[0]][c[1]]){
+                continue;
+            }else {
+                
+                if (c[0] == rl-1 && c[1] == cl-1){
+                    min = Math.min(min, c[2]);
+                    break;
+                }
+                
+                isVisited[c[0]][c[1]] = true;
+                
+                for (int[] direction : drdc) {
+                    rowI = c[0] + direction[0];
+                    columnI = c[1] + direction[1];
+                    if (checkIndex(rowI, columnI)){
+                        child.add(new int[]{rowI, columnI, c[2] + 1});
+                    }
+                }
             }
         }
-        isVisited[rowI][columnI] = false;
         
         return min;
     }
-    
+
     public boolean checkIndex(int rowI, int columnI){
         if (rowI < 0 || rowI >=rl || columnI < 0 || columnI >= cl)
             return false;
