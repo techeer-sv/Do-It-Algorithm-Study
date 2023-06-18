2b60add515102bb5a4a7ea20f36064ae5d93d5f5
김서영 <55133538+ksy990628@users.noreply.github.com>
2023-06-18 17:34:41 +0900

    무한루프 방지용 최대 이동횟수 변수 추가

--- seoyeong/code/Week10.java
+++ seoyeong/code/Week10.java
@@ -1,3 +1,6 @@
+// 두 큐 합 같게 만들기
+// https://school.programmers.co.kr/learn/courses/30/lessons/118667
+
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -8,25 +11,30 @@ public class DoitJava_Week10 {
         System.out.println(ans1);
         int ans2 = solution(new int[]{1,2,1,2},new int[]{1,10,1,2}); // 7
         System.out.println(ans2);
-        int ans3 = solution(new int[]{1,1},new int[]{1,5}); // -1
+        int ans3 = solution(new int[]{1,4},new int[]{3,4}); // -1
         System.out.println(ans3);
-        int ans4 = solution(new int[]{1,4},new int[]{4,8}); // -1
-        System.out.println(ans4);
     }
+
 
     public static int solution(int[] queue1, int[] queue2) {
         int answer = 0;
-        // int [] => Integer [] => Queue 로 전환
-        Queue <Integer> q1 = new LinkedList<>(Arrays.asList(Arrays.stream(queue1).boxed().toArray(Integer[]::new)));
-        Queue <Integer> q2 = new LinkedList<>(Arrays.asList(Arrays.stream(queue2).boxed().toArray(Integer[]::new)));
+        int maxMove = queue1.length*4;  // 최대 이동 횟수를 정해줘야함, 안그러면 무한루프트 걸릴 수도...
         long q1_sum = Arrays.stream(queue1).sum();
         long q2_sum = Arrays.stream(queue2).sum();
         long mid = (q1_sum + q2_sum)/2;
-        int e = 0;
 
         if ((q1_sum+q2_sum)%2!=0) return -1; // 중간값이 홀수이면, 불가능
 
-        while (q1_sum != mid && q1_sum > 0 && q2_sum > 0){
+        // int [] => Integer [] => Queue 로 전환
+        Queue <Integer> q1 = new LinkedList<>(Arrays.asList(Arrays.stream(queue1).boxed().toArray(Integer[]::new)));
+        Queue <Integer> q2 = new LinkedList<>(Arrays.asList(Arrays.stream(queue2).boxed().toArray(Integer[]::new)));
+        int e = 0;
+
+        while (q1_sum != mid){
+            if (q1_sum <= 0 || q2_sum <= 0)
+                return -1;
+            if (answer >= maxMove) 
+                return -1;
             // 두 큐 중에 값이 더 부족한 곳으로 이동
             if (q1_sum < mid){
                 e = q2.poll();
@@ -42,10 +50,6 @@ public class DoitJava_Week10 {
             }
             answer ++;
         }
-
-        if (q1_sum == 0 || q2_sum == 0)
-            return -1;
-
         return answer;
     }
 }
