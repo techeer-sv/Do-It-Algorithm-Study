5bd9996cba270969e8b77876a07589f9b9a7aff9
Jung Yunho <a01049048063@gmail.com>
2023-06-18 15:40:30 +0900

    feat: add problem solution

---
+++ Yunho/code/week10/Solution.java
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/118667
+// 두 큐의 원소를 같게하기
+
+import java.util.*;
+
+
+class Solution {
+    /*
+        목표: 각 queue가 (모든 원소의 합)/2가 되도록 하는 최소 횟수를 구해야함
+        1. 모든 큐의 원소의 합을 구한다 
+        2. 모든 원소의 합이 홀수이면 -1을 return
+        3. queue1과 queue2를 비교해서 
+            3-1. queue1 > queue2이면 queue1의 원소를 poll해서 queue2의 원소에 offer
+            3-2. 위의 케이스와 반대로 
+        4.
+    */
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -2;
+        return answer;
+    }
+}
