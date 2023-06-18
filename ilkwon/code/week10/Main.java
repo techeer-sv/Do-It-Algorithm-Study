e0ab0a2ff79e83507414dbb22fdeeda553e19b0b
1ilkwon <84134727+1ilkwon@users.noreply.github.com>
2023-06-18 23:54:31 +0900

    chore: Solve the problem related at algorithm

---
+++ ilkwon/code/week10/Main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+
+        Queue<Integer>q1 = new LinkedList<>();
+        Queue<Integer>q2 = new LinkedList<>();
+        long sum1 = 0;
+        long sum2 = 0;
+        int len = queue1.length;
+
+        for(int i=0; i<len; i++){
+            sum1 += queue1[i];
+            sum2 += queue2[i];
+            q1.add(queue1[i]);
+            q2.add(queue2[i]);
+        }
+
+        while(sum1 != sum2){
+            answer++;
+
+            if(sum1 > sum2){
+                int q1value = q1.poll();
+                sum1 -= q1value;
+                sum2 += q1value;
+                q2.add(q1value);
+            }else{
+                int q2value = q2.poll();
+                sum1 += q2value;
+                sum2 -= q2value;
+                q1.add(q2value);
+            }
+
+            if(answer > (queue1.length + queue2.length) * 2) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
