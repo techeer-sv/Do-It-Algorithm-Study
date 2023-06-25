0636afa6b84739377c86693268cd5a31b2494121
김서영 <55133538+ksy990628@users.noreply.github.com>
2023-06-26 01:11:18 +0900

    Update Week11.java

--- seoyeong/code/Week11.java
+++ seoyeong/code/Week11.java
@@ -8,6 +8,8 @@ import java.util.*;
 
 public class DoitJava_Week11 {
     /*
+10 20 14 18 17
+
 5
 10 -1
 10 1 -1
@@ -16,14 +18,13 @@ public class DoitJava_Week11 {
 3 3 -1
     */
 
-    // 10 20 14 18 17
     public static void main(String[]args) throws IOException {
         // 변수
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<List<Integer>> buildings = new ArrayList<>();
-        int [] ans = new int[n]; // 정답용 (최단시간)
-        Queue<Integer> queue = new LinkedList<>(); // 시간 계산용 큐
+        List<List<Integer>> buildings = new ArrayList<>(); // 빌딩 짓는 데 걸리는 시간 및 선행되어야 하는 건물
+        List<List<Integer>> orders = new ArrayList<>(); // 선행되어야 하는 건물
+        int [] ans = new int[n]; // 건물 당 최단시간
 
         // 입력
         for (int i = 0; i < n; i++) {
@@ -34,40 +35,46 @@ public class DoitJava_Week11 {
             }
             buildings.add(Arrays.asList(t2));
             ans[i] = (buildings.get(i).get(0));
-            queue.add(i);
         }
 
-        // 계산
-        // queue = {0,1,2,3,4}
-        while(queue.size()>0){
-            int x = queue.poll(); // idx pop
-            int cnt = 0;
-            for (int i=1; i < buildings.get(x).size(); i++){
-                Integer a = buildings.get(x).get(i) - 1; // 선행되어야 하는 인덱스
-                if (a == -2) {
-                    cnt = 0;
-                    break;
-                }
-                if (!queue.contains(a)){
-                    ans[x] = ans[a] + ans[x];
-                    cnt ++;
-                    continue;
-                }
-                else{
-                    queue.add(x);
-                    break;
+        // orders (선행되어야 하는 건물 순서) 정리하기
+        for (int i=0; i<n; i++){
+            if (buildings.get(i).size() == 2){
+                orders.add(new ArrayList<>());
+                continue;
+            }
+            ArrayList<Integer> o = new ArrayList<>();
+            for (int j=1; j<buildings.get(i).size()-1; j++)
+                o.add(buildings.get(i).get(j)-1);
+            orders.add(o);
+        }
+
+        for (int i=0; i<n; i++){
+            if (orders.get(i).size()>1){
+                List<Integer> o = orders.get(i);  // [2,0]
+                for (int j=0; j<o.size(); j++){
+                    int f = o.get(j);
+                    for (int k=o.size()-1; k>j; k--){
+                        if (orders.get(f).contains(o.get(k)))
+                            orders.get(i).remove(k);
+                    }
                 }
             }
-            for (int k=0; k<cnt; k++)
-                buildings.get(x).remove(1);
+        }
 
-
-
-            // ans
-            for (int a : ans){
-                System.out.print(a+" ");
+        // 최단 시간 계산 (orders를 중심으로)
+        int idx = 0;
+        while (idx < n){
+            for (int i=0; i<orders.get(idx).size(); i++){
+                ans[idx] += ans[orders.get(idx).get(i)];
             }
-            System.out.println();
+            idx++;
+        }
+
+        
+        // 정답 출력
+        for (int a : ans){
+            System.out.println(a);
         }
     }
 }
