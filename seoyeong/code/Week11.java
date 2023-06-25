643abad739f8bb2c24832d3ede94c5e13d27ad52
김서영 <55133538+ksy990628@users.noreply.github.com>
2023-06-26 00:02:22 +0900

    Update Week11.java

--- seoyeong/code/Week11.java
+++ seoyeong/code/Week11.java
@@ -4,9 +4,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class DoitJava_Week11 {
     /*
@@ -17,15 +15,17 @@ public class DoitJava_Week11 {
 4 3 1 -1
 3 3 -1
     */
+
+    // 10 20 14 18 17
     public static void main(String[]args) throws IOException {
         // 변수
-        List<List<Integer>> buildings = new ArrayList<>();
-        ArrayList<Integer> ans = new ArrayList<>(); // 정답용 (최단시간)
-
-        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        List<List<Integer>> buildings = new ArrayList<>();
+        int [] ans = new int[n]; // 정답용 (최단시간)
+        Queue<Integer> queue = new LinkedList<>(); // 시간 계산용 큐
 
+        // 입력
         for (int i = 0; i < n; i++) {
             String[] t1 = br.readLine().split(" ");
             Integer [] t2 = new Integer[t1.length];
@@ -33,20 +33,41 @@ public class DoitJava_Week11 {
                 t2[j] = Integer.parseInt(t1[j]);
             }
             buildings.add(Arrays.asList(t2));
-            ans.add(buildings.get(i).get(0));
+            ans[i] = (buildings.get(i).get(0));
+            queue.add(i);
         }
 
         // 계산
-        // while (buildings.add())
+        // queue = {0,1,2,3,4}
+        while(queue.size()>0){
+            int x = queue.poll(); // idx pop
+            int cnt = 0;
+            for (int i=1; i < buildings.get(x).size(); i++){
+                Integer a = buildings.get(x).get(i) - 1; // 선행되어야 하는 인덱스
+                if (a == -2) {
+                    cnt = 0;
+                    break;
+                }
+                if (!queue.contains(a)){
+                    ans[x] = ans[a] + ans[x];
+                    cnt ++;
+                    continue;
+                }
+                else{
+                    queue.add(x);
+                    break;
+                }
+            }
+            for (int k=0; k<cnt; k++)
+                buildings.get(x).remove(1);
 
 
-        // buildings
-        for (int i=0; i<buildings.size(); i++)
-            System.out.println(buildings.get(i).toString());
 
-        // ans
-        for (int i=0; i<buildings.size(); i++)
-            System.out.println(ans.get(i).toString());
-
+            // ans
+            for (int a : ans){
+                System.out.print(a+" ");
+            }
+            System.out.println();
+        }
     }
 }
