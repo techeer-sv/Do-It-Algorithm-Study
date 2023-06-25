f2f70a9032f9153fd9258f953f523709e0eb9659
Jung Yunho <a01049048063@gmail.com>
2023-06-25 15:06:46 +0900

    feat: add problem solution

---
+++ Yunho/code/week11/Main.java
@@ -0,0 +1,20 @@
+package Yunho.code.week11;
+//https://www.acmicpc.net/problem/1516
+//게임 개발
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    /**
+     * 1. 첫줄에서 받은 값만큼의 큐를 선언함 
+     * 2. 다음 줄 부터 받은 내용을 각 큐에 저장을 함
+     * 3. 큐에서 첫번째 요소 더하기 (-1)이 나올때 까지 각 번쨰의 큐의 첫 번째 요소 합을 순차적으로 출력함? 
+     * 
+     * 
+     *  (고려사항) 다른 큐에서 선행적으로 이루어져야 하는 큐를 요구할 경우 합이 꼬이는 경우가 있을 거 같은데 방법을 생각중
+     */
+	public static void main(String[] args) {
+		
+	}
+}
