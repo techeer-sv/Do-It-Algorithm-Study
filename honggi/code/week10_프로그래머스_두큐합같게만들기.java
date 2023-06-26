cada8cea808b3304d6fdd519445924c180ba9670
OhHongGi <5ghdrl@naver.com>
2023-06-26 17:35:05 +0900

    feat: Solve the problem
    Issue num : #71 (week 10 - Common problem solution)

--- honggi/code/week10_프로그래머스_두큐합같게만들기.java
+++ honggi/code/week10_프로그래머스_두큐합같게만들기.java
@@ -1,16 +1,18 @@
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Arrays;
 
 class Solution {
-    int min = Integer.MAX_VALUE;
     int qL;
+    Long goal;
     public int solution(int[] queue1, int[] queue2) {
         /*
-            url - https://school.programmers.co.kr/learn/courses/30/lessons/118667
+            1. 두 개의 int array를 Queue 자료구조에 담고, 각 Queue의 합을 구하기
+            2. 두 Queue의 각 합이 동일할 때까지 count를 늘려가며 반복
+                1) count가 queue의 크기의 4배가 되는 경우 -> 두 큐의 원소의 합이 같을 수 없으므로 -1
+                2) 두 큐의 원소의 합 중, 큰 쪽의 큐 원소를 삭제해 다른 큐에 삽입
             
-            1. 두 큐의 합 구하기.
-            2. 둘 중 하나의 큐에서 pop, 다른 queue에 insert (dfs)
-            3. 두 과정을 반복, 합이 같으면 종료
         */
         Long q1s = 0L, q2s = 0L;
         qL = queue1.length;
@@ -23,37 +25,37 @@ class Solution {
             q2s += (long) queue2[i];
             q2.add((long) queue2[i]);
         }
-        int answer = dfs(q1, q2, q1s, q2s, 0);
-        return (answer != Integer.MAX_VALUE) ? answer : -1;
+
+        if ((q1s + q2s) % 2 == 1)
+            return -1;
+        else
+            goal = (q1s + q2s)/2;
+        
+        return calculateCount(q1, q2, q1s, q2s);
     }
     
-    public int dfs(Queue<Long> q1, Queue<Long> q2, Long q1s, Long q2s, int count){
-        if (q1s == q2s){
-            System.out.println("q1S:" + q1s+ ", q2S:" + q2s);
-            min = Math.min(min, count);
-            return min;
-        } else if (count == qL*4){
-            return min;
-        } else {
-            return Math.min(pop(q1, q2, q1s, q2s, count), 
-                                pop(q2, q1, q2s, q1s, count));
+    public int calculateCount(Queue<Long> q1, Queue<Long> q2, Long q1s, Long q2s){
+        int count = 0;
+        
+        while (true){
+            if (count == qL*4){
+                return -1;
+            } else {
+                if (q1s > q2s){
+                    Long e = q1.poll();
+                    q2.add(e);
+                    q1s -= e;
+                    q2s += e;
+                } else if (q1s < q2s){
+                    Long e = q2.poll();
+                    q1.add(e);
+                    q2s -= e;
+                    q1s += e;
+                } else {
+                    return count;
+                }
+                count++;
+            }
         }
     }
-    public int pop(Queue<Long> q1, Queue<Long> q2, Long q1s, Long q2s, int count){
-        if (q1.isEmpty())
-            return Integer.MAX_VALUE;
-        else{
-            Long q1e = q1.remove();
-            q2.add(q1e);
-            return dfs(q1, q2, q1s - q1e, q2s + q1e, count+1);
-        }
-    }    
-    
-    public String print(Queue<Long> q){
-        while (!q.isEmpty()){
-            System.out.print(q.poll() +" ");
-        }
-        return "\n";
-    }
-    
 }
