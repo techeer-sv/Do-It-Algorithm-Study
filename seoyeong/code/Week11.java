159123c0192128c550d7d7fca8692976c5784344
김서영 <55133538+ksy990628@users.noreply.github.com>
2023-07-02 19:21:21 +0900

    Update Week11.java

--- seoyeong/code/Week11.java
+++ seoyeong/code/Week11.java
@@ -1,10 +1,17 @@
 // 게임 개발
 // https://www.acmicpc.net/problem/1516
 
+// 위상정렬
+// 진입차수가 0인 노드를 큐에 넣는다.
+// 큐가 빌 때까지 다음의 과정을 반복한다.
+//① 큐에서 원소를 꺼내 해당 노드에서 나가는 간선을 그래프에서 제거
+//② 새롭게 진입차수가 0이 된 노드를 큐에 삽입
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+
 
 public class DoitJava_Week11 {
     /*
@@ -18,15 +25,37 @@ public class DoitJava_Week11 {
 3 3 -1
     */
 
+
+    /*
+2 6 2 4 2
+
+5
+2 -1
+2 1 5 -1
+2 -1
+2 3 -1
+2 -1
+*/
+
+        /*
+2 10 6 4 2
+
+5
+2 -1
+2 1 3 5 -1
+2 4 -1
+2 5 -1
+2 -1
+*/
+
+
     public static void main(String[]args) throws IOException {
         // 변수
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         List<List<Integer>> buildings = new ArrayList<>(); // 빌딩 짓는 데 걸리는 시간 및 선행되어야 하는 건물
-        List<List<Integer>> orders = new ArrayList<>(); // 선행되어야 하는 건물
-        int [] ans = new int[n]; // 건물 당 최단시간
 
-        // 입력
+        // 입력 + buildings + ans
         for (int i = 0; i < n; i++) {
             String[] t1 = br.readLine().split(" ");
             Integer [] t2 = new Integer[t1.length];
@@ -34,47 +63,39 @@ public class DoitJava_Week11 {
                 t2[j] = Integer.parseInt(t1[j]);
             }
             buildings.add(Arrays.asList(t2));
-            ans[i] = (buildings.get(i).get(0));
         }
 
-        // orders (선행되어야 하는 건물 순서) 정리하기
-        for (int i=0; i<n; i++){
-            if (buildings.get(i).size() == 2){
-                orders.add(new ArrayList<>());
+
+        // 위상 정렬
+        Queue<Integer> queue = new LinkedList<>();
+        int cnt = 0; // 빌딩을 짓는 데 걸리는 총 시간
+        int [] visited = new int[n]; // 이미 선행된 빌딩이었는지 확인
+        for (int i=0; i<n; i++) {
+            // 진입 차수가 0일 때
+            if (buildings.get(i).size()<3){
+                System.out.println(buildings.get(i).get(0));
                 continue;
             }
-            ArrayList<Integer> o = new ArrayList<>();
-            for (int j=1; j<buildings.get(i).size()-1; j++)
-                o.add(buildings.get(i).get(j)-1);
-            orders.add(o);
-        }
 
-        for (int i=0; i<n; i++){
-            if (orders.get(i).size()>1){
-                List<Integer> o = orders.get(i);  // [2,0]
-                for (int j=0; j<o.size(); j++){
-                    int f = o.get(j);
-                    for (int k=o.size()-1; k>j; k--){
-                        if (orders.get(f).contains(o.get(k)))
-                            orders.get(i).remove(k);
+            // 진입 차수가 0보다 클 때
+            queue.add(i);
+            while (!queue.isEmpty()) {
+                int k = queue.poll();
+                if (visited[k] == 0) {
+                    cnt += buildings.get(k).get(0); // 빌딩을 짓는 데 걸리는 기본 시간
+                    visited[k] = 1;
+                    int j = 1;
+                    while (buildings.get(k).get(j) > -1) {
+                        int next = buildings.get(k).get(j)-1; // 인덱스 값이라 -1 해줘야함
+                        if (visited[next] == 0) queue.add(next);
+                        j++;
                     }
+
                 }
             }
-        }
-
-        // 최단 시간 계산 (orders를 중심으로)
-        int idx = 0;
-        while (idx < n){
-            for (int i=0; i<orders.get(idx).size(); i++){
-                ans[idx] += ans[orders.get(idx).get(i)];
-            }
-            idx++;
-        }
-
-        
-        // 정답 출력
-        for (int a : ans){
-            System.out.println(a);
+            System.out.println(cnt);
+            cnt = 0;
+            Arrays.fill(visited,0);
         }
     }
 }
