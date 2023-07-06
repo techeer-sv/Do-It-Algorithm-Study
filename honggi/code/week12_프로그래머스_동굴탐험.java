809adf6f29bf5a376b54678aa02652459b43afb9
OhHongGi <5ghdrl@naver.com>
2023-07-06 17:41:17 +0900

    feat: Solve the problem
    Issue num : #87 (week 12 - Common problem solution)

--- honggi/code/week12_프로그래머스_동굴탐험.java
+++ honggi/code/week12_프로그래머스_동굴탐험.java
@@ -1,8 +1,7 @@
 import java.util.LinkedList;
-import java.util.Arrays;
 import java.util.Queue;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
 
 class Solution {
     /*
@@ -10,81 +9,109 @@ class Solution {
         탐험이 가능한지. (0번 방부터 탐험 시작)
         
         주의사항
-        - 모든 방은 양방향으로 연결
         - 먼저 방문해야 할 방이 겹치는 경우는 x
+        - 임의의 서로 다른 두 방 사이의 최단경로는 딱 한 가지 -> cycle이 존재하지 않음
         
         1. 각 방마다 연결 가능한 통로 정보, order 정보를 정리하여 저장
-                
+        2. 0번방을 queue에 삽입
+        3. queue의 head를 제거하여 연결된 방을 탐색
+            1) 해당 방이 order에 포함된 방인 경우([A,B]중 B에 속할 때) 
+              -> A를 방문할 때 까지 기다리도록 Set에 저장
+            2) 해당 방이 order에 포함된 방인 경우([A,B]중 A에 속할 때)
+              ① Set에서 B를 삭제 및 queue에 삽입
+              ② 자신의 depth보다 높은 방들을 queue에 삽입
+            3) 해당 방이 order에 포함되지 않는 방(전제조건 x)인 경우
+              -> 자신의 depth보다 높은 방들을 queue에 삽입
+        4. queue가 빌 때 까지 3번 반복
     */
     LinkedList<Integer>[] paths;
     int[] orderInfo;
     Queue<Integer> queue;
-    Map<Integer, Integer> findOrder;
-    boolean[] visited;
+    HashMap<Integer, Integer> findOrder;
+    HashSet<Integer> waitRoom;
+    int[] degree;
     public boolean solution(int n, int[][] path, int[][] order) {
-        paths = new LinkedList[n];
+        paths = new LinkedList[n];  
         queue = new LinkedList();
+        waitRoom = new HashSet();
         orderInfo = new int[n];
         findOrder = new HashMap<Integer, Integer>();
-                
+        degree = new int[n];
+        
+        // 리스트 초기화
+        for (int i = 0; i<n; i++){
+            paths[i] = new LinkedList();
+        }
+
+        // 각 방마다 연결 가능한 통로 정보 정리
         for (int[] p : path){
-            if (paths[p[0]] == null){
-                paths[p[0]] = new LinkedList();
-            }
             paths[p[0]].add(p[1]);
-            
-            if (paths[p[1]] == null){
-                paths[p[1]] = new LinkedList();
-            }            
             paths[p[1]].add(p[0]);
         }
-                
+        
+        // 진입차수 정리
+        sortDegree();
+        
+        // order 정보 정리
         for (int[] o : order){
             orderInfo[o[1]] = o[0];
             findOrder.put(o[0], o[1]);
         }
         
-        if (findOrder.containsValue(0))
-            return false;
-                
-        for (int i = 0; i < Math.abs(n); i++){
-            visited = new boolean[n];
-            exploration();
-        }
+        exploration(n);
         
-        boolean answer = true;                
-        for (int i = 0; i<n; i++){
-            if (!visited[i]){
-                answer = false;
-                break;
-            }
-        }
-        
-        return answer;
+        // order가 모두 삭제된 경우 -> 모든 방을 탐색한 경우.
+        return findOrder.isEmpty();
     }
     
-    public void exploration(){
+    // 진입차수 정리 (0번 방을 root로 하고, tree의 depth로 이용)
+    public void sortDegree(){
         queue.add(0);
-        visited[0] = true;
+        
+        while (!queue.isEmpty()){
+            int i = queue.remove();
+            LinkedList<Integer> path = paths[i];
+            
+            for (int p : path){
+                // 0번 방이 아니거나 아직 차수가 정리되지 않은 방 -> 방 i보다 depth가 깊은 방
+                if (p != 0 && degree[p] == 0){
+                    degree[p] = degree[i] + 1;
+                    queue.add(p);
+                } 
+            }
+        }
+    }
+    
+    // 탐사 진행
+    public void exploration(int n){
+        queue.add(0);
         
         while (!queue.isEmpty()){
             int room = queue.remove();
-            LinkedList<Integer> rooms = paths[room];
-            for (int r : rooms){
-                if (!visited[r]){
-                    Integer o = findOrder.remove(r);
-                    
-                    if (o != null){
-                        orderInfo[o] = 0;
-                        queue.clear();
-                        break;
+            
+            // 먼저 방문해야할 방 조건이 없는 경우
+            if (orderInfo[room] == 0){
+                Integer o = findOrder.remove(room);
+                
+                // 해당 방을 방문함으로써 방문할 수 있는 방이 생기는 경우
+                if (o != null){ 
+                    orderInfo[o] = 0;
+                    // 방문하지 못했던 방이 존재할 때 -> queue 삽입
+                    if (waitRoom.remove(o)){
+                        queue.add(o);
                     }
-                    
-                    if (orderInfo[r] == 0){
-                        visited[r] = true;
-                        queue.add(r);
-                    }               
                 }
+
+                // 자신의 depth보다 높은 방들을 queue에 삽입
+                LinkedList<Integer> rooms = paths[room];
+                for (int r : rooms){
+                    if (degree[room] < degree[r]){
+                        queue.add(r);
+                    }
+                }
+            } else {
+                // 먼저 방문해야할 방을 방문할 때 까지 대기
+                waitRoom.add(room);
             }
         }
     }
