19d47987efe60e58be61a2cda3a86c6a3ead8c67
김서영 <55133538+ksy990628@users.noreply.github.com>
2023-07-13 22:46:51 +0900

    Update Week13.java

--- seoyeong/code/Week13.java
+++ seoyeong/code/Week13.java
@@ -1,73 +1,27 @@
 // Predict the Winner
 // https://leetcode.com/problems/predict-the-winner/
+// 두 player는 모두 최적의 결과를 생각하며 대결
 
-// 두 player는 모두 최적의 결과를 얻기 위해 대결
-// player 2명이 nums 배열에서 원하는 인덱스(0, 앞)(n-1, 뒤)를 하나씩 뽑아 각 본인의 숫자에 더하는 것
-// player 1이 이길 수 있거나 비길 수 있으면 return true
-
-class Solution {
-    public boolean calculateNum(int p1, int p2, List<Integer>n){
-switch (n.size()){
-            case 0:
-                if (p1>=p2) return true;
-                return false;
-            case 1:
-                if (p1+n.get(0)>=p2) return true;
-                return false;
-            case 2:
-                if (p1+n.get(0)>=p2+n.get(1) || p1+n.get(1)>=p2+n.get(0)) return true;
-                return false;
-            case 3:
-                int x = n.get(0);
-                int y = n.get(1);
-                int z = n.get(2);
-                if (x>=y){
-                    if (y>=z){
-                        n.remove(0); n.remove(0);
-                        return calculateNum(p1+x,p2+y,n);
-                    }
-                    n.remove(0); n.remove(1);
-                    return calculateNum(p1+x,p2+z,n);
-                }
-                else{
-                    if (x>=y){
-                        n.remove(0); n.remove(1);
-                        return calculateNum(p1+z,p2+x,n);
-                    }
-                    n.remove(1); n.remove(1);
-                    return calculateNum(p1+z,p2+y,n);
-                }
-        }
-
-
-        int front1 = n.get(0);
-        int front2 = n.get(1);
-        int back1 = n.get(n.size()-1);
-        int back2 = n.get(n.size()-1);
-
-        // player1이 앞 + player2가 앞
-        n.remove(0); n.remove(0);
-        boolean chooseFrontFront = calculateNum(p1+front1,p2+front2,n);
-        // player1이 앞 + player2가 뒤
-        n.add(0,front2); n.remove(n.size()-1);
-        boolean chooseFrontBack = calculateNum(p1+front1,p2+back1,n);
-        // player1이 뒤 + player2가 앞
-        boolean chooseBackFront = calculateNum(p1+back1, p2+front1, n);
-        // player1이 뒤 + player2가 뒤
-        n.add(0,front1); n.remove(n.size()-1);
-        boolean chooseBackBack = calculateNum(p1+back1, p2+back2, n);
-
-        if (chooseFrontFront == true || chooseFrontBack == true || chooseBackFront == true || chooseBackBack == true) return true;
-        return false;
+public class DoitJava_Week13 {
+    public static void main(String[] args) {
+        System.out.println(PredictTheWinner(new int[]{1,5,2})); // false
+        System.out.println(PredictTheWinner(new int[]{1,5,233,7})); // true
+        System.out.println(PredictTheWinner(new int[]{1,2,99})); // true
+        System.out.println(PredictTheWinner(new int[]{2,4,55,6,8})); // false
     }
 
-    public boolean PredictTheWinner(int[] nums) {
-        int player1 = 0;
-        int player2 = 0;
-        List<Integer> n = new ArrayList<>();
-        for (int x : nums) n.add(x);
+    public static boolean PredictTheWinner(int [] nums){
+        return getWinner(nums, 0, nums.length-1) >= 0;
+    }
 
-        if (calculateNum(player1,player2,n) == true) return true;
-        return false;
+    // start와 end는 인덱스
+    public static int getWinner(int [] nums, int start, int end) {
+        if (start == end)
+            return nums[end];   // start == end == 0, length == 1일 때
+
+        // 둘 다 음수 값인데 그 중에서도 최댓값으로
+        int fromFirst = nums[start] - getWinner(nums, start+1, end);
+        int fromSecond = nums[end] - getWinner(nums, start, end-1);
+        return Math.max(fromFirst, fromSecond);
     }
 }
