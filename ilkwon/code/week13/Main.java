9e60e793f976bc2403e1a0b822069ea49387d65f
1ilkwon <84134727+1ilkwon@users.noreply.github.com>
2023-07-16 18:07:51 +0900

    fix : 문제 다시 해석
    재귀를 통한 코드 풀이 확인 후 풀이

--- ilkwon/code/week13/Main.java
+++ ilkwon/code/week13/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+/*public class Main {
     public static void main(String[] args) {
         int[] nums = {1,5,2};
         PredictTheWinner(nums);
@@ -53,4 +53,24 @@ public class Main {
         return answer;
     }
 
+}*/
+
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        return fun(nums,0,nums.length-1,0,0,1); // turn = 플레이어 차례
+    }
+
+    boolean fun(int[] arr,int start,int end,int sum1,int sum2,int turn){
+        if(start>end){ //더 이상 선택할 숫자가 없을 때
+            if(sum1 >= sum2) return true; //플레이어 1이 이긴 경우
+            else return false; //플레이어 2가 이긴 경우
+        }
+
+        if(turn == 1) //플레이어 1 : 둘중 하나라도 성립하는 경우 (위 코드에서) true 반환
+            return fun(arr,start+1,end,sum1+arr[start],sum2,1-turn) || //첫번째를 선택한 경우.
+                    fun(arr,start,end-1,sum1+arr[end],sum2,1-turn); // 마지막을 선태택한 경우.
+        else //플레이어 2 : 두 경우 모두 성립하는 경우 true 반환
+            return fun(arr,start+1,end,sum1,sum2+arr[start],1-turn) &&
+                    fun(arr,start,end-1,sum1,sum2+arr[end],1-turn);
+    }
 }
