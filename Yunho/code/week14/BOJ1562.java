f435c6b58ba64bd99cd33e4d283d49e655263197
Jung Yunho <a01049048063@gmail.com>
2023-07-25 21:40:05 +0900

    feat: stiil solving BOJ1562

---
+++ Yunho/code/week14/BOJ1562.java
@@ -0,0 +1,21 @@
+//https://www.acmicpc.net/problem/1562
+//계단수 
+import java.util.*;
+import java.io.*;
+/**
+ * 문제에서 "첫째 줄에 정답을 1,000,000,000으로 나눈 나머지를 출력한다." -> 버퍼오버플로우를 주의해라! 
+ * 
+ */
+
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+
+
+}
