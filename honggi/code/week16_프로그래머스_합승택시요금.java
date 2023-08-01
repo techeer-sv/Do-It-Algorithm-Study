2f57d5f454f51fdc1282089b0c1a9402bb885e61
OhHongGi <5ghdrl@naver.com>
2023-08-01 12:37:11 +0900

    chore: Solving the problem
    Improve accuracy
    
    Issue num : #123 (week 16 - Common problem solution)

--- honggi/code/week16_프로그래머스_합승택시요금.java
+++ honggi/code/week16_프로그래머스_합승택시요금.java
@@ -1,63 +1,70 @@
 import java.util.HashMap;
 import java.util.Map.Entry;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Arrays;
-
+import java.util.PriorityQueue;
 
 class Solution {
+    
+    /*
+        모든 지점에서 a와 b를 도착하는데 드는 택시비용 계산 -> dijkstra 이용
+    */
     class Fee{
-        int dst;
+        int loc;
         int fee;
         
-        public Fee(int d, int f){
-            this.dst = d;
+        public Fee(int l, int f){
+            this.loc = l;
             this.fee = f;
         }
     }
-    int[] dp;
+    
+    int N;
+    int dp[];
     HashMap<Integer, Integer>[] maps;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        dp = new int[n+1];
         maps = new HashMap[n+1];
+        N = n;
         
-        for(int i = 0; i<n; i++){
+        for(int i = 1; i<=n; i++){
             maps[i] = new HashMap(200);
         }
         
         int answer = 0;
         for (int i = 0; i<fares.length; i++){
             maps[fares[i][0]].put(fares[i][1], fares[i][2]);
+            maps[fares[i][1]].put(fares[i][0], fares[i][2]);
         }
         
+        dp = calculateFee(s);
+        int result = Integer.MAX_VALUE;
         for (int i = 1; i<=n; i++){
-            dp[i] = maps[s].getOrDefault(i, Integer.MAX_VALUE);
+            int[] dijkstra = calculateFee(i);
+            result = Math.min(dijkstra[a] + dijkstra[b] + dp[i], result);
         }
-        
-        int feeA = calculateFee(s, a);
-        System.out.println(Arrays.toString(dp));
-        int feeB = calculateFee(s, b);
-        System.out.println(Arrays.toString(dp));
-        
-        return answer;
+        return result;
     }
     
-    public int calculateFee(int start, int end){
-        Queue<Fee> queue = new LinkedList<>();
-        queue.add(new Fee(0,0));
+    // dijkstra 시간복잡도 - nlogn (힙구조 사용)
+    public int[] calculateFee(int start){
+        int dp[] = new int[N+1];
+        
+        for (int i = 1; i<=N; i++){
+            dp[i] = maps[start].getOrDefault(i, Integer.MAX_VALUE);
+        }
+
+        PriorityQueue<Fee> queue = new PriorityQueue<>((Fee a,Fee b) -> Integer.compare(a.fee, b.fee));
+        queue.add(new Fee(start,0));
         while (!queue.isEmpty()){
             Fee f = queue.remove();
             
-            if (dp[f.dst] < f.fee)
+            if (dp[f.loc] < f.fee)
                 continue;
             
-            for (Entry<Integer, Integer> e : maps[f.dst].entrySet()){
+            for (Entry<Integer, Integer> e : maps[f.loc].entrySet()){
                 dp[e.getKey()] = Math.min(dp[e.getKey()], e.getValue() + f.fee);
                 queue.add(new Fee(e.getKey(), e.getValue() + f.fee));
             }
         }
-        
-        return dp[end];
+        dp[start] = 0;
+        return dp;
     }
-    
 }
