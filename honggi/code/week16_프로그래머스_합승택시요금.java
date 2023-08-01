40d6f47226573693bf4d2c33569b7777cfeab606
OhHongGi <5ghdrl@naver.com>
2023-08-02 01:33:14 +0900

    feat: Solve the problem
    Issue num : #123 (week 16 - Common problem solution)

--- honggi/code/week16_프로그래머스_합승택시요금.java
+++ honggi/code/week16_프로그래머스_합승택시요금.java
@@ -1,11 +1,20 @@
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Arrays;
 
 class Solution {
     
     /*
-        모든 지점에서 a와 b를 도착하는데 드는 택시비용 계산 -> dijkstra 이용
+        출발지를 기준으로, 연결된 길을 걸어가며 A와 B에 도착하는 요금 중 최소값 구하기
+        1. 현재 위치에서 A와 B에 도착하는 요금 중 최소값 구하기 (그리디)
+        2. 출발지로부터 연결된 길 까지 공동 택시 요금을 구하며 하단의 과정 반복 (dp)
+            1) 현재 위치(queue.pop)까지의 공통 요금을 제외하고, A와 B의 목적지 도착 요금 구하기
+            2) 1에서 구한 값과, 이전 위치에서 구한 1)값을 비교하여 최소값 갱신
+            3) 현재 위치와 연결된 모든 길(돌아온 길 제외)을 queue에 삽입
+        
+        배운점
+        - Arrays.fill이 반복문보다 빠르다.
+        - Arrays.setAll은 내부 함수를 활용하여 다양하게 사용 가능 (dp, i -> i*2) = 0 2 4 ..
     */
     class Fee{
         int loc;
@@ -18,53 +27,86 @@ class Solution {
     }
     
     int N;
-    int dp[];
-    HashMap<Integer, Integer>[] maps;
+    LinkedList<Fee>[] maps;
+    int QSize;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        maps = new HashMap[n+1];
+        maps = new LinkedList[n+1];
         N = n;
+        QSize = (N*(N-1))/2+2;
         
         for(int i = 1; i<=n; i++){
-            maps[i] = new HashMap(200);
+            maps[i] = new LinkedList<Fee>();
         }
         
-        int answer = 0;
         for (int i = 0; i<fares.length; i++){
-            maps[fares[i][0]].put(fares[i][1], fares[i][2]);
-            maps[fares[i][1]].put(fares[i][0], fares[i][2]);
+            maps[fares[i][0]].add(new Fee(fares[i][1], fares[i][2]));
+            maps[fares[i][1]].add(new Fee(fares[i][0], fares[i][2]));
         }
         
-        dp = calculateFee(s);
-        int result = Integer.MAX_VALUE;
-        for (int i = 1; i<=n; i++){
-            int[] dijkstra = calculateFee(i);
-            result = Math.min(dijkstra[a] + dijkstra[b] + dp[i], result);
-        }
-        return result;
+        return commonFee(s, a, b);
     }
     
-    // dijkstra 시간복잡도 - nlogn (힙구조 사용)
-    public int[] calculateFee(int start){
-        int dp[] = new int[N+1];
+    public int commonFee(int start, int end1, int end2){
+        boolean[] visited = new boolean[N+1];
+        PriorityQueue<Fee> queue = new PriorityQueue<>(QSize, (a, b) -> a.fee - b.fee);
         
-        for (int i = 1; i<=N; i++){
-            dp[i] = maps[start].getOrDefault(i, Integer.MAX_VALUE);
-        }
+        int[] sFee = calculateFee(start, 0);
+        int result = sFee[end1] + sFee[end2];
 
-        PriorityQueue<Fee> queue = new PriorityQueue<>((Fee a,Fee b) -> Integer.compare(a.fee, b.fee));
-        queue.add(new Fee(start,0));
+        queue.add(new Fee(start, 0));
+        
         while (!queue.isEmpty()){
             Fee f = queue.remove();
             
-            if (dp[f.loc] < f.fee)
-                continue;
-            
-            for (Entry<Integer, Integer> e : maps[f.loc].entrySet()){
-                dp[e.getKey()] = Math.min(dp[e.getKey()], e.getValue() + f.fee);
-                queue.add(new Fee(e.getKey(), e.getValue() + f.fee));
+            if (!visited[f.loc]){
+                visited[f.loc] = true;
+
+                int[] aFee = calculateFee(f.loc, end1);
+                int[] bFee = calculateFee(f.loc, end2);
+
+                result = Math.min(sFee[f.loc] + aFee[end1] + bFee[end2], result);
+
+                for (Fee e : maps[f.loc]){
+                    if (!visited[e.loc] && result > f.fee + e.fee){
+                        queue.add(new Fee(e.loc, f.fee + e.fee));
+                    }
+                }
             }
         }
-        dp[start] = 0;
-        return dp;
+            
+        return result;
     }
+    
+    
+    // dijkstra 시간복잡도 - nlogn (힙구조 사용)
+    public int[] calculateFee(int start, int end){
+        boolean[] visited = new boolean[N+1];
+        int[] dp = new int[N+1];
+        
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[start] = 0; 
+
+        PriorityQueue<Fee> queue = new PriorityQueue<>(QSize, (a, b) -> a.fee - b.fee);
+        
+        queue.add(new Fee(start,0));
+        
+        while (!queue.isEmpty()){
+            Fee f = queue.remove();
+            
+            if (f.loc == end){
+                break;
+            }
+            if (!visited[f.loc]){
+                visited[f.loc] = true;
+
+                for (Fee e : maps[f.loc]){
+                    if (!visited[e.loc] && dp[e.loc] > e.fee + f.fee){
+                        dp[e.loc] = e.fee + f.fee;
+                        queue.add(new Fee(e.loc, e.fee + f.fee));
+                    }
+                }
+            }
+       }
+        return dp;
+    }    
 }
