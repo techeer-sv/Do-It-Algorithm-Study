f433ebc1a92be32bd9c2331ffccf287ee428de45
Leejiwon <dlwldnjs1009@gmil.com>
2023-08-06 20:02:03 +0900

    chore: week16_1학년

--- jiwon/code/week16/1학년.py
+++ jiwon/code/week16/1학년.py
@@ -1,19 +1,18 @@
 n = int(input())
-
-d = [[0, 0] for _ in range(n)]
 array = list(map(int, input().split()))
 
-count = 0
-d[0][0] = array[0]
-d[0][1] = array[0]
+d = [[0] * 21 for _ in range(n)]
+d[0][array[0]] = 1
 
-for i in range(n-1):
-    for j in range(2):
-        if j == 0:
-            d[i + 1][j] = d[i][j] - array[i]
-        elif j == 1:
-            d[i + 1][j] = d[i][j] + array[i]
-        if array[-1] == d[-2][0] or array[-1] == d[-2][1]:
-            count += 1
+for i in range(1, n-1):
+    for j in range(21):
+        if d[i-1][j] > 0:
+            if j + array[i] <= 20:
+                d[i][j+array[i]] += d[i-1][j]
+            if j - array[i] >= 0:
+                d[i][j-array[i]] += d[i-1][j]
 
-print(count)
+
+
+
+print(d[n-2][array[-1]])
