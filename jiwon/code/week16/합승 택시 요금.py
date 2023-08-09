3daf5951c05d42dbfa341fc963a7ce1682ee3754
Leejiwon <dlwldnjs1009@gmil.com>
2023-08-09 20:48:56 +0900

    chore:week16_합승_택시_요금

--- jiwon/code/week16/합승 택시 요금.py
+++ jiwon/code/week16/합승 택시 요금.py
@@ -1,6 +1,5 @@
 def solution(n, s, a, b, fares):
     INF = int(1e9)
-    answer = 0
     graph = [[INF] * (n+1) for _ in range(n+1)]
     for i in range(1, n+1):
         graph[i][i] = 0
@@ -9,7 +8,12 @@ def solution(n, s, a, b, fares):
         graph[x][y] = z
         graph[y][x] = z
 
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
+    answer = graph[s][a] + graph[s][b]
+    for i in range(1, n+1):
+        answer = min(answer, graph[s][i] + graph[i][a] + graph[i][b])
     return answer
-
-solution(6,4,6,2,[[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]])
